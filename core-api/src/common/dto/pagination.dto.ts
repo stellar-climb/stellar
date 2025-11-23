46865ba70865bfc4cbae2bd013a07e671ccb7af0
김정호 <jeangho293@naver.com>
2025-11-24 00:02:36 +0900

    fix: example

--- core-api/src/common/dto/pagination.dto.ts
+++ core-api/src/common/dto/pagination.dto.ts
@@ -2,11 +2,17 @@ import { IsNumber, IsOptional } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class PaginationDto {
+  /**
+   * @example 1
+   */
   @Type(() => Number)
   @IsNumber()
   @IsOptional()
   page?: number = 1;
 
+  /**
+   * @example 10
+   */
   @Type(() => Number)
   @IsNumber()
   @IsOptional()
