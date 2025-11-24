ac10fb3244f6f19154698ff0e4385fbfea80119e
theo7727 <theo@dobedub.com>
2025-11-24 11:57:04 +0900

    fix

--- core-api/src/swagger/generate-swagger.ts
+++ core-api/src/swagger/generate-swagger.ts
@@ -1,4 +1,4 @@
-import { AppModule } from '../src/app.module';
+import { AppModule } from '../app.module';
 import { NestFactory } from '@nestjs/core';
 import { PaginationDto } from '@common/dto';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
