a5e598e6b8ebcec96fa1e90107d15083b8811efd
김정호 <jeangho293@naver.com>
2025-11-30 01:47:26 +0900

    feat: user

--- core-api/src/services/users/controllers/general-user.controller.ts
+++ core-api/src/services/users/controllers/general-user.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { UserGuard } from '@common/guards';
+import { Context, ContextKey } from '@common/context';
 
 @ApiTags('[사용자] 사용자 API')
 @Controller('users')
 @UseGuards(UserGuard)
 export class GeneralUserController {
+  constructor(private readonly context: Context) {}
+
   /**
    * 사용자 목록 조회
    */
@@ -13,8 +16,10 @@ export class GeneralUserController {
   self() {
     // 1. Destructure body, params, query
     // 2. Get context
+    const user = this.context.get(ContextKey.USER);
+
     // 3. Get result
     // 4. Send response
-    return {};
+    return { data: user };
   }
 }
