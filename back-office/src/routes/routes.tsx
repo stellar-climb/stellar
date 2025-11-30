910b8fc7f3bc75d74552a405f767273e192b487e
김정호 <jeangho293@naver.com>
2025-11-30 13:31:00 +0900

    fix: auth router

--- back-office/src/routes/routes.tsx
+++ back-office/src/routes/routes.tsx
@@ -1,12 +1,44 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Layout } from './base-layout';
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { useUser } from '@libs';
+
+function AuthorizedRoute() {
+  // 1. destructure props
+  // 2. lib hooks
+  const [user] = useUser();
+
+  // 3. state hooks
+  // 4. query hooks
+  // 5. form hooks
+  // 6. calculate values
+  // 7. effect hooks
+  // 8. handlers
+  return user ? <Navigate to="/" /> : <Outlet />;
+}
+
+function UnauthorizedRoute() {
+  // 1. destructure props
+  // 2. lib hooks
+  const [user] = useUser();
+
+  // 3. state hooks
+  // 4. query hooks
+  // 5. form hooks
+  // 6. calculate values
+  // 7. effect hooks
+  // 8. handlers
+  return user ? <Navigate to="/" /> : <Outlet />;
+}
 
 export function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
+        <Route element={<AuthorizedRoute />}>
           <Route path="/" element={<div>Home</div>} />
+        </Route>
+
+        <Route element={<UnauthorizedRoute />}>
+          <Route path="/login" element={<div>fdasfsafdsafdsafdsafdas</div>} />
         </Route>
       </Routes>
     </BrowserRouter>
