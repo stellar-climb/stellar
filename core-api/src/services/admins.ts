f7ce9e459d6ce576556c82b38adce9ed9b11c120
김정호 <jeangho293@naver.com>
2025-12-08 21:05:02 +0900

    feat: 모듈

--- core-api/src/services/admins.ts
+++ core-api/src/services/admins.ts
@@ -2,5 +2,6 @@ import { AdminUserModule } from './users/admin-user.module';
 import { AdminModule } from './admins/admins.module';
 import { AdminAuthModule } from './auth/admin-auth.module';
 import { AdminRoleModule } from './roles/admin-role.module';
+import { AdminRolePolicyModule } from './role-policy/admin-role-policy.module';
 
-export default [AdminUserModule, AdminModule, AdminAuthModule, AdminRoleModule];
+export default [AdminUserModule, AdminModule, AdminAuthModule, AdminRoleModule, AdminRolePolicyModule];
