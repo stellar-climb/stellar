fba40d77efebee905a56f530971f1bc6d6db6b56
theo7727 <theo@dobedub.com>
2026-01-27 15:41:08 +0900

    fix: tag

--- core-api/src/services/music/admin-music.module.ts
+++ core-api/src/services/music/admin-music.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { MusicRepository } from './repository/music.repository';
 import { AdminMusicController } from './controllers/admin-music.controller';
 import { AdminMusicService } from './applications/admin-music.service';
+import { TagRepository } from '@services/tags/repository/tag.repository';
 
 @Module({
   controllers: [AdminMusicController],
-  providers: [MusicRepository, AdminMusicService],
+  providers: [MusicRepository, AdminMusicService, TagRepository],
   exports: [MusicRepository, AdminMusicService],
 })
 export class AdminMusicModule {}
