2b294abd258ccfec40f7379bdf3a4833c918fcf8
김정호 <jeangho293@naver.com>
2026-02-21 22:21:03 +0900

    이렇게 하면 되나

--- back-office/src/libs/http-client/index.ts
+++ back-office/src/libs/http-client/index.ts
@@ -4,7 +4,7 @@ export const httpClient = (() => {
   let _authorization = '';
 
   const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_CORE_API_URL,
+    baseURL: 'admins',
     headers: {
       Authorization: '',
     },
